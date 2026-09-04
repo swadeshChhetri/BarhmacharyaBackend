4ed6a9e292c6825867c9068578228703289fb972
Swadesh Chhetri <163563708+swadeshChhetri@users.noreply.github.com>
2026-09-04 11:04:27 +0530

    allow the another origin

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
+      "https://brahmacharyaa.vercel.app",
       "https://bhramacharya.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
